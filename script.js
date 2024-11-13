c53ad1f80a36b8c0d8bf7f0d4ca117af8043977d
Izaankhan0 <izaankhan.x@gmail.com>
2024-11-13 12:05:17 +0500

    added nav and burger for mobile

--- script.js
+++ script.js
@@ -98,3 +98,16 @@ function wheel() {
 function textChange() {
     document.querySelector('.text-change-1').innerHTML = `${wheelPos} / 4`
 }
+
+let flag = false;
+let nav = document.querySelector('.van')
+function switch1() {
+  if (flag == false) {
+    nav.classList.add('show');
+    flag = true;
+  }
+  else if (flag == true) {
+    nav.classList.remove('show');
+    flag = false;
+  }
+}
